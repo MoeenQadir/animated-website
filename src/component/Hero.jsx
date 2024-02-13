670c44b55b50b460dc4d747f313de4b7c041965c
MoeenQadir <myselfxdeveloper@gmail.com>
2024-02-13 23:48:22 +0500

    basic structure completed

---
+++ src/component/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </>
+    );
+};
+
+export default Hero;
