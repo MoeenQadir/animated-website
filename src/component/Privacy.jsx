2ac1a7ea86dfe2dd0e53e26da5d59543655a1196
MoeenQadir <myselfxdeveloper@gmail.com>
2024-02-15 00:19:06 +0500

    hotfix

--- src/component/Privacy.jsx
+++ src/component/Privacy.jsx
@@ -6,16 +6,16 @@ import {FaDiscord, FaTwitter} from "react-icons/fa";
 const Privacy = () => {
     return (
         <>
-            <div className="relative bg-[#18153a]">
+            <div className="relative bg-[#181539] lg:bg-none">
                 <img
-                    className="absolute inset-0 w-full lg:h-screen lg:h-auto object-contain"
+                    className="absolute inset-0 w-full lg:h-screen"
                     style={{"height":"auto"}}
                     src="/assetes/screens/screen5.png"
                     alt="hero background image"
                 />
                 <div className="relative mx-auto">
                     <div className="mb-12 text-center space-y-4 md:mb-20 md:pt-32 lg:w-11/12 lg:mx-auto">
-                            <Button  className="rounded-full mt-6 bg-[#ffffca] text-bold text-black">
+                            <Button  className="rounded-full my-16 bg-[#ffffca] text-bold text-black">
                                 Join the Movement
                             </Button>
                             <div className={"w-full flex justify-center"}>
@@ -36,13 +36,13 @@ const Privacy = () => {
                             </div>
                     </div>
                     <div className={"w-full text-center"}>
-                    <h3 className={"text-white text-2xl md:text-3xl lg:mt-32 mt-8 lg:text-4xl font-bold"}>Share Your Story</h3>
+                    <h3 className={"text-white text-2xl md:text-3xl lg:mt-32 lg:text-4xl font-bold"}>Share Your Story</h3>
                         <h5 className={"text-white mt-6"}>Type your story below and be a catalyst for change. Your words matter, and your voice makes a <br/> difference. Together, let's create a future filled with shared dreams and collective progress</h5>
                     </div>
                     <div className={"container mx-auto"}>
                         <h3 className={"text-white ms-4 md:ms-8 lg:ms-64 text-2xl md:text-3xl lg:mt-32 mt-16 lg:text-4xl font-bold"}>Describe yourself</h3>
                         <div className={"lg:w-9/12 w-11/12 mx-auto flex justify-center flex-col items-center"}>
-                            <textarea className="resize-x text-card mt-6 h-80 w-full lg:w-10/12 p-5 text-white shadow-md" placeholder={"Tell us about yourself here...."}></textarea>
+                            <textarea className="resize-x text-card mt-6 h-96 w-full lg:w-10/12 p-5 text-white shadow-md" placeholder={"Tell us about yourself here...."}></textarea>
                             <div className={"flex justify-end w-full me-4 lg:me-44  text-white mt-4"}>
                                 <p>Minimum 1024 Characters</p>
                             </div>
@@ -52,7 +52,7 @@ const Privacy = () => {
                                 </Button>
                             </div>
                         </div>
-                       <footer className={"mt-80 text-white w-full flex flex-col items-center justify-center"}>
+                       <footer className={"mt-40 text-white w-full flex flex-col items-center justify-center"}>
                            <h3>Terms of Service</h3>
                            <div className={"flex space-x-4"}>
                                <span className={"h-8 w-8 rounded-full mt-8 flex justify-center items-center border-2 border-white"}><BsInstagram /></span>
